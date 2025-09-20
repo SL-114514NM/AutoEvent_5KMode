a6a39a849db9753d436e45c6da6523dfca897cfd
114514 <3145186196@qq.com>
2025-09-20 20:37:01 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoEvent_5KMode
+{
+    public class Class1
+    {
+    }
+}
